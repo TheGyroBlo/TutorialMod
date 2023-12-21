1cb153ef422d601a8594bf6e90262fd9dbd8bd5c
TheGyroBlo <150994565+TheGyroBlo@users.noreply.github.com>
2023-12-20 23:03:35 -0500

    Init commit7.

--- TutorialMod.cs
+++ TutorialMod.cs
@@ -28,14 +28,14 @@ public class TutorialMod : CementMod
       CanSwitch = false;
       foreach(Actor guy in FindObjectsOfType<Actor>())
     {
-        float r = UnityEngine.Random.Range(1,255);
-        float g = UnityEngine.Random.Range(1,255);
-        float b = UnityEngine.Random.Range(1,255);
+        // float r = UnityEngine.Random.Range(1,255);
+        // float g = UnityEngine.Random.Range(1,255);
+        // float b = UnityEngine.Random.Range(1,255);
 
-        Vector4 newColor = new Vector4(r,g,b,1);
+        // Vector4 newColor = new Vector4(r,g,b,1);
         
-        guy.primaryColor = newColor;
-        guy.CostumeColor = newColor;
+        guy.primaryColor = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        guy.CostumeColor = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
         
     }
     Invoke("ResetColorChange", 0.5f);
