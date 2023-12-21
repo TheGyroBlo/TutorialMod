163d60d48dd705246616ea810df4f26cad97140a
TheGyroBlo <150994565+TheGyroBlo@users.noreply.github.com>
2023-12-20 22:53:52 -0500

    Init commit4.

--- TutorialMod.cs
+++ TutorialMod.cs
@@ -6,24 +6,41 @@ using Femur;
 // This is an example Mod class
 public class TutorialMod : CementMod 
 {
+   bool CanSwitch = false;
 
     private void Awake()
     {
+    
     }
-    private void Start(){
-
- }
+    private void Start()
+    {
+      CanSwitch = true;
+    }
 
     private void Update()
- {
-    foreach(Actor guy in FindObjectsOfType<Actor>())
     {
-        guy.primaryColor = Color.blue;
+      if(CanSwitch){
+         ChangeColor();
+      }
     }
+    
+   private void ChangeColor(){
+      CanSwitch = false;
+      foreach(Actor guy in FindObjectsOfType<Actor>())
+    {
+        float r = UnityEngine.Random.Range(1,255);
+        float g = UnityEngine.Random.Range(1,255);
+        float b = UnityEngine.Random.Range(1,255);
 
+        Vector4 newColor = new Vector4(r,g,b,.5f);
+        
+        guy.primaryColor = newColor;
+        guy.CostumeColor = newColor;
+        Invoke("ResetColorChange", 0.5f);
+    }
+   }
+
+   private void ResetColorChange(){
+      CanSwitch = true;
+   }
  }
-
- 
-
-
-}
