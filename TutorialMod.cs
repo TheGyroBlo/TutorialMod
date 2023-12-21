9fc45751b90f294444160193216c74b0463835c5
TheGyroBlo <150994565+TheGyroBlo@users.noreply.github.com>
2023-12-20 22:42:54 -0500

    Init commit4.

--- TutorialMod.cs
+++ TutorialMod.cs
@@ -1,10 +1,29 @@
 using System;
 using UnityEngine;
 using CementTools;
+using Femur;
 
 // This is an example Mod class
 public class TutorialMod : CementMod 
 {
+
+    private void Awake()
+    {
+    }
+    private void Start(){
+
+ }
+
+    private void Update()
+ {
+    foreach(Actor guy in FindObjectsOfType<Actor>())
+    {
+        guy.primaryColor = Color.blue;
+    }
+
+ }
+
+ 
 
 
 }
